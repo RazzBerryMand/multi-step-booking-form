0ac8f3d84674701848fbdd93897473c299c07fa3
Mand Cashin <amanda.cashin@magairports.com>
2021-09-23 15:05:04 +0100

    feat: add next page button and submit button

--- src/components/FormPage/formPage.js
+++ src/components/FormPage/formPage.js
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import "./formPage.css";
 import ProgressBar from "./progressBar";
 
 function FormPage() {
+  const [page, setPage] = useState(1);
+
+  const nextPage = () => {
+    if (page === 4) return;
+    setPage((page) => page + 1);
+  };
+
   return (
     <div>
       <ProgressBar />
-      <p>content</p>
+
+      {page === 1 && <p>entry time, flight details, exit time, pax numbers</p>}
+      {page === 2 && <p>choose carpark</p>}
+      {page === 3 && <p>choose extras</p>}
+      {page === 4 && <p>summary and payment</p>}
+
+      {page !== 4 && <button onClick={nextPage}>Next step</button>}
+      {page === 4 && <button type="submit">Submit</button>}
     </div>
   );
 }
